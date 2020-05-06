368fd2c25c282d729eec53eb03960eb57e4c8db1
Peer Naupold <greenion@gmx.de>
2020-05-06 04:49:05 +0200

    added gitignore file and changed SECRET_KEY

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'accounts'
+    'accounts',
 ]
 
 MIDDLEWARE = [
--- env.py
+++ env.py
@@ -1,3 +1,3 @@
 import os
-os.environ.setdefault("SECRET_KEY", "mq(6ctmt$1v779m^k=5vw_&g=yj#bypo5yo3$g-uv$ix1bi_3u")
+os.environ.setdefault("SECRET_KEY", "3=s(d^0$$n0jgwcpuak-@8dvw)@ywa!pz^kf+whotl*f-k!f=a")
 os.environ.setdefault('LOCAL_HOSTNAME', '127.0.0.1')
