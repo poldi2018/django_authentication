0e34478fd2ebab88146076ee9ffd3d4bd4c5fea3
Peer Naupold <greenion@gmx.de>
2020-05-06 06:13:12 +0200

    changes made in settings.py

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -135,7 +135,7 @@ MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
 """ 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gail.com'
+EMAIL_HOST = 'smtp.gmail.net'
 EMAIL_HOST_USER = os.getenv('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
 EMAIL_PORT = 587 """
